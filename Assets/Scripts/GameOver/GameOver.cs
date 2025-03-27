6ff7cadef7cff60ec1818e3663bb4eb572733ac6
maykenm7 <141855292+ymartinezsmartsoft@users.noreply.github.com>
2025-03-27 15:38:03 -0500

    UI y Escena Sencilla Game Over
    Cambio de la fuente a la UI y Escena Sencilla de Game Over.

---
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,26 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+
+    public void ShowGameOver()
+    {
+        Time.timeScale=0;
+        gameOverPanel.SetActive(true);        
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale=1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale=1;
+        SceneManager.LoadScene("HomeScene");
+    }
+}
