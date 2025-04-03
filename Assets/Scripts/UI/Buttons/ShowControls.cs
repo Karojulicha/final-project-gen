2c0ebcfc39a69790f3b97af32af72e5f9f445ffa
Oidenhiops <juanzateorozco@gmail.com>
2025-04-02 23:15:23 -0500

    Se corrigen errores varios

--- Assets/Scripts/UI/Buttons/ShowControls.cs
+++ Assets/Scripts/UI/Buttons/ShowControls.cs
@@ -7,14 +7,19 @@ public class ShowControls : MonoBehaviour
     // Referencias a los paneles
     public GameObject controlsPanel;
     public GameObject dialogPanel;
-
-    // Textos de los diálogos
-    public string[] dialogTexts;
+    public DialogsInfo[] dialogs;
     public TMP_Text dialogText;
 
     private int currentDialogIndex = 0;
 
     public ManagementCharacter managementCharacter;
+
+    void Start()
+    {
+        foreach(var dialog in dialogs){
+            dialog.dialog = ManagementData.Instance.GetDialog(dialog.id, ManagementData.Instance.saveData.configurationsInfo.currentLanguage.ToString());
+        }
+    }
 
     // Método para cerrar el panel de controles
     public void CloseControls()
@@ -42,15 +47,24 @@ public class ShowControls : MonoBehaviour
     // Método para mostrar el siguiente diálogo
     private void ShowNextDialog()
     {
-        if (currentDialogIndex < dialogTexts.Length)
+        if (currentDialogIndex < dialogs.Length)
         {
-            dialogText.text = dialogTexts[currentDialogIndex];
+            dialogText.text = dialogs[currentDialogIndex].dialog;
             currentDialogIndex++;
         }
         else
         {
             dialogPanel.SetActive(false);
             managementCharacter.canPlay = true;
+        }
+    }
+    [System.Serializable] public class DialogsInfo{
+        public int id;
+        public string dialog;
+        public DialogsInfo(int id, string dialog)
+        {
+            this.id = id;
+            this.dialog = dialog;
         }
     }
 }
